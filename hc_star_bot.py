1d81d56ce50572dc26d7c7822c7dd63d61fa66e5
HarshHC <harshchandrahc5@gmail.com>
2018-10-25 07:40:12 +0530

    Fixed errors , added features

--- hc_star_bot.py
+++ hc_star_bot.py
@@ -13,15 +13,17 @@ def handler(bot, update):
     api = requests.get(url=request_url)
     response = api.json()
     request = update.message.text
+    result1 = ""
     result = ""
     for data in response:
         if data['name'] != request:
             continue
-        result = "Repository '%s' has %d Stars and %d Forks" % (request, data['stargazers_count'],data[forks_count])
+        result1 = "Repository '%s' has %d Stars and %d Forks" % (request, data['stargazers_count'],data['forks_count'])
+        result = "Repository not found."
         break
     if result == "":
-        result = "Repository not found."
-    update.message.reply_text(result)
+        result1 = "Repository not found."
+    update.message.reply_text(result1)
 
 
 dispatcher.add_handler(MessageHandler(Filters.text, handler))
